972de4bc4690d5b69c4ecead105b21c0e4fe3c17
youngho kim <yhkim052556@naver.com>
2025-06-30 22:22:36 +0900

    프로젝트 초기 세팅, 멀티 모듈 세팅

---
+++ demo-api/src/test/java/team9/demo/RoomGenieApplicationTests.java
@@ -0,0 +1,13 @@
+package team9.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoomGenieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
