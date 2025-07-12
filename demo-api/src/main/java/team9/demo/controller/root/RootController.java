c684da2f03cd988ab33aa4355da4fbf0ae9c8e4b
youngho kim <yhkim052556@naver.com>
2025-07-13 01:52:52 +0900

    배포를 위한 설정 완료

---
+++ demo-api/src/main/java/team9/demo/controller/root/RootController.java
@@ -0,0 +1,12 @@
+package team9.demo.controller.root;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RootController {
+    @GetMapping("/health")
+    public String healthCheck() {
+        return "I'm healthy!";
+    }
+}
